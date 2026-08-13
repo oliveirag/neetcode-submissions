de1a7725837ba3417da68bf5d579f8c7311dda84
oliveirag <113860797+oliveirag@users.noreply.github.com>
2026-08-13 19:11:44 -0400

    Add: python-scope - submission-0

---
+++ Python For Beginners/python-scope/submission-0.py
@@ -0,0 +1,11 @@
+def add_one(n):
+    n = n + 1
+    print(n)   
+
+n = 10
+
+add_one(n)     # Output: 11
+
+print(n)       # Output: ?
+
+# output is still 10 because n is declared outside the function
