93bf88224441337f93a628d6ed07138eb8442ad3
oliveirag <113860797+oliveirag@users.noreply.github.com>
2026-08-17 18:05:46 -0400

    Add: python-more-operators - submission-2

---
+++ Python For Beginners/python-more-operators/submission-2.py
@@ -0,0 +1,7 @@
+a, b, c = 2, 8, 5
+
+product = a * b # create variable
+print(product // c) # floor division
+print(product % c) # modulus
+print(a ** b) # exponentiation
+print(b ** c) # exponentiation
