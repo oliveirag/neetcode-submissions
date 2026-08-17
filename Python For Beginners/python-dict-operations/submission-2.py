9d7e964ed3b3f352b39b55dd42110ba9438d40e1
oliveirag <113860797+oliveirag@users.noreply.github.com>
2026-08-17 19:41:53 -0400

    Add: python-dict-operations - submission-2

---
+++ Python For Beginners/python-dict-operations/submission-2.py
@@ -0,0 +1,11 @@
+your_dict = { 
+  "a": 10, 
+  "apple": 12,
+  "bat": 7
+}
+
+print(your_dict) # print dictionary
+print(your_dict["a"]) # print the value of key "a"
+print("d" in your_dict) # print true or false whether the key "d" is in the dictionary
+your_dict["a"] = 4 # reassign the value of the key "a" to 4
+print(your_dict) # print dictionary again
