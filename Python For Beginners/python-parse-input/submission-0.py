b34f1cbc5f90999f0c1a09ac876d592c91f8f2c8
oliveirag <113860797+oliveirag@users.noreply.github.com>
2026-08-17 20:25:31 -0400

    Add: python-parse-input - submission-0

---
+++ Python For Beginners/python-parse-input/submission-0.py
@@ -0,0 +1,10 @@
+from typing import List
+
+def read_integers() -> List[int]:
+    res = input()
+    
+
+# do not modify the code below
+print(read_integers())
+print(read_integers())
+print(read_integers())
