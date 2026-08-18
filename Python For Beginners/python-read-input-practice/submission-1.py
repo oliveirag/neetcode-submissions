052bdf354ed49e18354c490e79db18569783756a
oliveirag <113860797+oliveirag@users.noreply.github.com>
2026-08-17 20:48:00 -0400

    Add: python-read-input-practice - submission-1

---
+++ Python For Beginners/python-read-input-practice/submission-1.py
@@ -0,0 +1,14 @@
+def add_two_numbers() -> int:
+    res = input() # take user input
+    numbers = res.split(",") # create list of substrings
+    result = numbers[0] + numbers[1]
+    return result
+
+
+
+
+# do not modify below this line
+print(add_two_numbers())
+print(add_two_numbers())
+print(add_two_numbers())
+print(add_two_numbers())
