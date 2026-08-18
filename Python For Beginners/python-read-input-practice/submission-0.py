b240ff6cc1b42adfa39af4d8adf3967bc34192ee
oliveirag <113860797+oliveirag@users.noreply.github.com>
2026-08-17 20:44:29 -0400

    Add: python-read-input-practice - submission-0

---
+++ Python For Beginners/python-read-input-practice/submission-0.py
@@ -0,0 +1,7 @@
+def read_and_print_input() -> None:
+    user_input = input("Reading one line of text into a string: ") # read user input
+    print(user_input) # print the input
+
+# add the function calls
+read_and_print_input()
+read_and_print_input()
