6626ba0bbaa0f5710c571756ec4d224f55e7c42a
oliveirag <113860797+oliveirag@users.noreply.github.com>
2026-08-19 17:53:54 -0400

    Add: python-double-ended-queue - submission-2

---
+++ Python Coding Interviews/python-double-ended-queue/submission-2.py
@@ -0,0 +1,16 @@
+from typing import List, Deque
+from collections import deque
+
+def rotate_list(arr: List[int], k: int) -> Deque[int]:
+    queue = deque(arr) # create queue using values from arr
+    for i in range(k): # loop k times
+        queue.appendleft(queue.pop()) # remove from right and add to the left (rotate numbers to the right)
+    return queue
+
+# do not modify below this line
+print(rotate_list([1, 2, 3, 4, 5], 0))
+print(rotate_list([1, 2, 3, 4, 5], 1))
+print(rotate_list([1, 2, 3, 4, 5], 2))
+print(rotate_list([1, 2, 3, 4, 5], 3))
+print(rotate_list([1, 2, 3, 4, 5], 4))
+print(rotate_list([1, 2, 3, 4, 5], 5))
