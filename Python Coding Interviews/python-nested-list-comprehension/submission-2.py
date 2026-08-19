ab1040eaf16394679542ba0bdd855bf7b5282c37
oliveirag <113860797+oliveirag@users.noreply.github.com>
2026-08-19 18:32:37 -0400

    Add: python-nested-list-comprehension - submission-2

---
+++ Python Coding Interviews/python-nested-list-comprehension/submission-2.py
@@ -0,0 +1,13 @@
+from typing import List
+
+# list comprehension
+def create_grid(rows: int, cols: int, value: int) -> List[List[int]]:
+    grid = [[value] * cols for i in range(rows)] # "[value] * cols" creates the first list of values
+    return grid                                  # "for _ in range(rows)" multiplies that list by rows
+                                                 # the _ is a throwaway loop variable
+# do not modify below this line
+print(create_grid(2, 3, 0))
+print(create_grid(3, 2, 1))
+print(create_grid(4, 4, 4))
+print(create_grid(1, 1, 5))
+print(create_grid(1, 5, 5))
