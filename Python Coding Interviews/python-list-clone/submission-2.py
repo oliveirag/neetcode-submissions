125e3e21ec84b9179085ab08b8dbaa06268b4bd8
oliveirag <113860797+oliveirag@users.noreply.github.com>
2026-08-19 12:09:55 -0400

    Add: python-list-clone - submission-2

---
+++ Python Coding Interviews/python-list-clone/submission-2.py
@@ -0,0 +1,22 @@
+from typing import List
+
+
+def remove_element(arr: List[int], element: int) -> List[int]:
+    cloned_arr = arr.copy() # make a copy of arr
+    cloned_arr = arr[:] # another way to make copy of arr
+    cloned_arr = list(arr) # another way to make copy of arr
+    for values in arr: # loop thru each value in arr
+        if element in cloned_arr: # if this value is in cloned_arr
+            cloned_arr.remove(element) # remove value from cloned_arr
+    return cloned_arr # return new list
+
+
+# do not modify below this line
+arr = [1, 3, 5, 7, 9]
+
+print(remove_element(arr, 3))
+print(arr)
+print(remove_element(arr, 9))
+print(arr)
+print(remove_element(arr, 1))
+print(arr)
