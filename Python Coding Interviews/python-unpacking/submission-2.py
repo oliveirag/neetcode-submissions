79e68e1a06bb677a359cbc8d83e5c315494022a0
oliveirag <113860797+oliveirag@users.noreply.github.com>
2026-08-18 20:05:33 -0400

    Add: python-unpacking - submission-2

---
+++ Python Coding Interviews/python-unpacking/submission-2.py
@@ -0,0 +1,19 @@
+from typing import List, Tuple
+
+
+def sum_3_integers(triplet: List[int]) -> int:
+    val1, val2, val3 = triplet
+    sum = val1 + val2 + val3
+    return sum
+
+def compute_volume(box_dimensions: Tuple[int, int, int]) -> int:
+    pass
+  
+
+# do not modify below this line
+print(sum_3_integers([1, 2, 3]))
+print(sum_3_integers([4, 6, 2]))
+
+print(compute_volume((1, 2, 3)))
+print(compute_volume((3, 2, 1)))
+print(compute_volume((3, 9, 7)))
