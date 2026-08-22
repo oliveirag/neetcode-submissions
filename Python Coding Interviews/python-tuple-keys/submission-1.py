0d3211af21d0d8cbf04978b0f480f6e42d912554
oliveirag <113860797+oliveirag@users.noreply.github.com>
2026-08-21 23:01:45 -0400

    Add: python-tuple-keys - submission-1

---
+++ Python Coding Interviews/python-tuple-keys/submission-1.py
@@ -0,0 +1,29 @@
+from typing import List, Set, Tuple
+
+
+def grid_to_set(grid: List[List[int]]) -> Set[Tuple[int, int]]:
+    my_set = set() # create set
+
+    for r in range((len(grid))): # loop thru each row
+        for c in range(len(grid[0])): # loop thru each column
+            if grid[r][c] == 1: # if current cell has a value of 1
+                my_set.add((r, c)) # add the coordinates as a tuple
+    return my_set # return the set of tuples
+
+# do not modify below this line
+
+output1 = grid_to_set([[1, 0, 1], [0, 1, 0], [1, 0, 1]])
+print(type(output1))
+print(sorted(list(output1)))
+      
+output2 = grid_to_set([[1, 0, 0], [0, 0, 0]])
+print(type(output2))
+print(sorted(list(output2)))
+
+output3 = grid_to_set([[1, 1, 1], [1, 1, 1]])
+print(type(output3))
+print(sorted(list(output3)))
+
+output4 = grid_to_set([[0, 0, 0], [0, 0, 0], [0, 0, 0]])
+print(type(output4))
+print(sorted(list(output4)))
