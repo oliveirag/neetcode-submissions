55bd435a0bc23cad55a27a4907503837ee3c13a7
oliveirag <113860797+oliveirag@users.noreply.github.com>
2026-08-21 22:52:52 -0400

    Add: python-hash-set-basics - submission-4

---
+++ Python Coding Interviews/python-hash-set-basics/submission-4.py
@@ -0,0 +1,42 @@
+from typing import List, Set
+
+
+def build_hash_set(keys: List[str]) -> Set[str]:
+    my_set = set() # create set
+
+    for i in range(len(keys)): # for each string in the list
+        my_set.add(keys[i]) # add the string to the set
+    return my_set # return the set
+
+def check_keys(hash_set: Set[str], keys: List[str]) -> List[bool]:
+    exists = []
+
+    for i in range(len(keys)): # loop thru each key
+        if keys[i] in hash_set: # if the key is in the hash set
+            exists.append(True) # add true to the list
+        else:                   # if key is not in hash set
+            exists.append(False) # add false to the list
+    return exists # return list of booleans
+    
+    # Alternative solution 2:
+    # output = []
+    # for key in keys:
+    #   output.append(key in hash_set)
+    # return output
+
+    # Alternative solution 2:
+    # return [key in hash_set for key in keys]
+
+# do not modify below this line
+
+output1 = build_hash_set(["Alice", "Bob", "Charlie"])
+print(type(output1))         # check the type of the output
+print(sorted(list(output1))) # set order is not guaranteed so we need to sort the list
+
+output2 = build_hash_set(["XY", "XX", "YY", "XY", "YX"]) 
+print(type(output2))         # check the type of the output
+print(sorted(list(output2))) # set order is not guaranteed so we need to sort the list
+
+print(check_keys({"Alice", "Bob", "Charlie"}, ["Alice", "Bob", "Charlie", "David"]))
+print(check_keys({'a', 'b', 'c'}, ['a', 'd', 'c']))
+print(check_keys({'a', 'c'}, ['d', 'c']))
