b3427b3a705d16aa62bcd42e6c0780e9a4c47242
oliveirag <113860797+oliveirag@users.noreply.github.com>
2026-08-21 22:23:21 -0400

    Add: python-default-dict - submission-4

---
+++ Python Coding Interviews/python-default-dict/submission-4.py
@@ -0,0 +1,31 @@
+from collections import defaultdict
+from typing import List, Dict
+
+def count_chars(s: str) -> Dict[str, int]:
+    freq = defaultdict(int) # set default value for keys that don't exist in dictionary
+
+    for char in s: # for each character
+        freq[char] += 1 # add it to the dictionary and assign its value
+
+    return freq # return the dictionary
+
+def nested_list_to_dict(nums: List[List[int]]) -> Dict[int, List[int]]:
+    d = defaultdict(list) # create dictionary where default value is an empty list
+
+    for sublist in nums: # for each sublist in nums
+        key = sublist[0] # set the key as the first element in each sublist
+
+        for i in range(1, len(sublist)): # loop thru each element in each sublist (skipping the key)
+            d[key].append(sublist[i]) # d[key] creates the empty list and .append adds each element
+
+    return d # return a dictionary that 
+
+# do not modify below this line
+print(count_chars("hello"))
+print(count_chars("helloworld"))
+print(count_chars("areallylongstringwhyareyoureadingthishahalol"))
+
+print(nested_list_to_dict([[1, 2, 3], [4, 5, 6], [1, 4]]))
+print(nested_list_to_dict([[1, 2, 3, 4], [4, 5, 6, 7], [1, 4, 5, 6]]))
+print(nested_list_to_dict([[5, 2, 3, 4, 5], [4, 5, 6, 7, 8], [5, 6, 7, 8, 9]]))
+print(nested_list_to_dict([[3, 2, 3, 4, 5], [4, 5, 6, 7, 8], [5, 6, 7, 8]]))
