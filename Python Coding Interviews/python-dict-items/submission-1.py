c4aa8883a1d65be61ea1eb9fb01f532f1899649a
oliveirag <113860797+oliveirag@users.noreply.github.com>
2026-08-21 22:37:58 -0400

    Add: python-dict-items - submission-1

---
+++ Python Coding Interviews/python-dict-items/submission-1.py
@@ -0,0 +1,24 @@
+from typing import Dict, List, Tuple
+
+
+def get_dict_items(age_dict: Dict[str, int]) -> List[Tuple[str, int]]:
+    res = [] # create list
+
+    for key, value in age_dict.items(): # for each key and value in age_dict
+        res.append((key, value)) # add key and value as a tuple
+
+    return res # return the list of tuples
+
+    # Alternative solution 1:
+    # return list(age_dict.items())
+
+    # Alternative solution 2:
+    # return [(key, value) for key, value in age_dict.items()]
+
+
+
+# do not modify below this line
+print(get_dict_items({'Alice': 25, 'Bob': 30, 'Charlie': 35}))
+print(get_dict_items({'Alice': 25, 'Bob': 30, 'Charlie': 35, 'David': 40}))
+print(get_dict_items({'Bob': 30, 'David': 40, 'Charlie': 35, 'Alice': 25, 'Eve': 45}))
+print(get_dict_items({'Alice': 25, 'Bob': 30, 'Charlie': 35, 'David': 40, 'Eve': 45, 'Frank': 50}))
